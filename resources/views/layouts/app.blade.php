e2c18ea98a1188472a99d7597ad288018b5f6345
Martin Dilling-Hansen <martindilling@gmail.com>
2018-08-16 23:51:22 +0200

    A little styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="flex h-full font-sans antialiased text-black leading-tight bg-grey-lighter">
-<div id="app" class="flex-1 block md:flex flex-col">
+<div id="app" class="flex-1 block md:flex flex-col border-t-4 border-indigo-lighter">
     {{--<nav class="flex items-center justify-between flex-wrap shadow-md bg-teal px-2 py-4">--}}
         {{--<a href="#" class="flex items-center flex-no-shrink text-white no-underline mr-4 px-2 rounded focus:outline-none focus:shadow-outline">--}}
             {{--<svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>--}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,16 @@
     <div class="h-full font-sans">
         <div class="h-full flex justify-center">
             <div class="w-full">
-                <h1 class="font-thin text-grey-dark mb-8 mt-6 text-center">
+                <h1 class="text-2xl text-indigo-darker font-bold my-10 text-center">
                     {{ __('Art by ') }}
                     <a href="http://martindilling.com"
-                       class="no-underline hover:underline cursor-default font-thin text-grey-dark">
+                       class="no-underline hover:underline cursor-default text-indigo-darker">
                         Martin Dilling-Hansen
                     </a>
                 </h1>
 
                 <div class="block lg:flex flex-wrap justify-center">
-                    <div class="w-full lg:w-1/2 bg-grey-light pl-4 lg:pl-24 pr-4 py-12">
+                    <div class="w-full lg:w-1/2 bg-grey-light pl-4 lg:pl-24 pr-4 py-24">
                         <div class="w-full lg:pr-5">
                             <div class="gumroad-product-embed flex justify-center items-center"
                                  data-gumroad-product-id="mdh_simple-symmetry">
@@ -24,7 +24,7 @@
                         </div>
                     </div>
 
-                    <div class="w-full lg:w-1/2 bg-grey-lighter lg:bg-grey-light pl-4 pr-4 lg:pr-24 py-12">
+                    <div class="w-full lg:w-1/2 bg-grey-lighter lg:bg-grey-light pl-4 pr-4 lg:pr-24 py-24">
                         <div class="w-full lg:pl-5">
                             <div class="gumroad-product-embed flex justify-center items-center"
                                  data-gumroad-product-id="mdh_invasion">
@@ -37,7 +37,7 @@
                 </div>
 
                 <div class="block lg:flex flex-wrap justify-center">
-                    <div class="w-full lg:w-1/2 bg-grey-light lg:bg-grey-lighter pl-4 lg:pl-24 pr-4 py-12">
+                    <div class="w-full lg:w-1/2 bg-grey-light lg:bg-grey-lighter pl-4 lg:pl-24 pr-4 py-24">
                         <div class="w-full lg:pr-5">
                             <div class="gumroad-product-embed flex justify-center items-center"
                                  data-gumroad-product-id="mdh_crossover">
@@ -48,7 +48,7 @@
                         </div>
                     </div>
 
-                    <div class="w-full lg:w-1/2 bg-grey-lighter pl-4 pr-4 lg:pr-24 py-12">
+                    <div class="w-full lg:w-1/2 bg-grey-lighter pl-4 pr-4 lg:pr-24 py-24">
                         <div class="w-full lg:pl-5">
                             <div class="gumroad-product-embed flex justify-center items-center"
                                  data-gumroad-product-id="mdh_holes">
